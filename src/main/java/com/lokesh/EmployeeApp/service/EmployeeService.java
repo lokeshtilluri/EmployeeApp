3010aa9bd771b27fafd5edf434d2b2c32ec100bd
Lokesh Tilluri <42713861+lokeshtilluri@users.noreply.github.com>
2023-12-16 16:04:31 +0530

    Revert "addEmployee changes"
    This reverts commit 0ec56ea6082e25a87328c5d2163ea9bd8f4b125c.

--- src/main/java/com/lokesh/EmployeeApp/service/EmployeeService.java
+++ src/main/java/com/lokesh/EmployeeApp/service/EmployeeService.java
@@ -6,7 +6,7 @@ import com.lokesh.EmployeeApp.payload.EmployeeDTO;
 import com.lokesh.EmployeeApp.payload.EmployeeResponse;
 
 public interface EmployeeService {
-	EmployeeDTO addEmployee(EmployeeDTO employeeDTO);
+	EmployeeResponse addEmployee(EmployeeDTO employeeDTO);
 	List<EmployeeResponse> getEmployees();
 	EmployeeResponse getEmployeeById(Long id);
 }
