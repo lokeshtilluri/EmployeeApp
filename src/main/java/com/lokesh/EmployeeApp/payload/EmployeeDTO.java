2292fa442ba5eb27f022f4b089e95546ea981b7d
Lokesh Tilluri <42713861+lokeshtilluri@users.noreply.github.com>
2023-12-17 15:21:21 +0530

    Phone number validation
    Phone number validation

--- src/main/java/com/lokesh/EmployeeApp/payload/EmployeeDTO.java
+++ src/main/java/com/lokesh/EmployeeApp/payload/EmployeeDTO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,7 +26,7 @@ public class EmployeeDTO {
 	@Email(message = "please enter valid email")
 	@Size(min=8,message="please enter valid email")
 	private String email;
-	private List<String> phoneNumbers;
+	private List< @Digits(integer = 10,fraction = 0,message = "Please Enter valid phone number") @Pattern(regexp = "^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$",message = "Enter valid 10 digit phone number") String> phoneNumbers;
 	private java.sql.Date DOJ;
 	@Digits(integer=20,fraction=0)
 	private Long salary;
